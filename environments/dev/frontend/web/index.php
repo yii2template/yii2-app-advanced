5a7bc7c80dbde1628003dd9deeb5d975e2722dd3
Vitaliy <theyamshikov@yandex.ru>
2018-10-04 22:01:09 +0600

    init rails

--- environments/dev/frontend/web/index.php
+++ environments/dev/frontend/web/index.php
@@ -1,17 +1,14 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
 
-require __DIR__ . '/../../vendor/autoload.php';
-require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
-require __DIR__ . '/../../common/config/bootstrap.php';
-require __DIR__ . '/../config/bootstrap.php';
+use yii2lab\app\App;
 
-$config = yii\helpers\ArrayHelper::merge(
-    require __DIR__ . '/../../common/config/main.php',
-    require __DIR__ . '/../../common/config/main-local.php',
-    require __DIR__ . '/../config/main.php',
-    require __DIR__ . '/../config/main-local.php'
-);
+$name = 'frontend';
+$path = '../..';
 
-(new yii\web\Application($config))->run();
+@include_once(__DIR__ . '/' . $path . '/vendor/yii2lab/yii2-app/src/App.php');
+
+if(!class_exists('yii2lab\app\App')) {
+	die('Run composer install');
+}
+
+App::run($name);
